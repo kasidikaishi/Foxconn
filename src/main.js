6cf489c87fa4e24c21bcf9c11cc621d173d95152
Jiefei Wang <wangjfmh@gmail.com>
2022-12-07 02:23:36 -0500

    add css file and optimise select value function

--- src/main.js
+++ src/main.js
@@ -9,43 +9,39 @@ function fetchData() {
       return response.json();
     })
     .then(data => {
+      const cityMap = new Map();
+      const typeMap = new Map();
+      let cityOptions = '';
+      let typeOptions = '';
+      for (let element of data) {
+        if (!cityMap.has(element.city)) {
+          cityMap.set(element.city, true);
+          cityOptions += `<option value=${element.city}>${element.city}</option>`
+        }
+        if (!typeMap.has(element.brewery_type)) {
+          typeMap.set(element.brewery_type, true);
+          typeOptions += `<option value=${element.brewery_type}>${element.brewery_type}</option>`
+        }
+      }
+
       const html = `
-        <div>
+        <div id="container">
           <h1 id="breweries-list">Breweries List</h1>
           <label htmlFor="cityFilter">City</label>
           <select id="cityFilter" name="cityFilter">
             <option value="AllCity">All</option>
-            <option value="San Diego">San Diego</option>
-            <option value="Knox">Knox</option>
-            <option value="Bend">Bend</option>
-            <option value="Boise">Boise</option>
-            <option value="Demver">Denver</option>
-            <option value="Porland">Porland</option>
-            <option value="Reno">Reno</option>
-            <option value="Quilcene">Quilcene</option>
-            <option value="Petaluma">Petaluma</option>
-            <option value="CastleRock">Castle Rock</option>
-            <option value="Anoka">Anoka</option>
-            <option value="Abington">Abington</option>
-            <option value="Houston">Houston</option>
-            <option value="JohnDay">John Day</option>
-            <option value="Killeshin">Killeshin</option>
-            <option value="Williamsville">Williamsville</option>
-            <option value="Gilbert">Gilbert</option>
-            <option value="Mesa">Mesa</option>
+            ${cityOptions}
           </select>
           <label htmlFor="typeFilter">Type</label>
           <select id="typeFilter" name="typeFilter">
             <option value="AllType">All</option>
-            <option value="micro">micro</option>
-            <option value="large">large</option>
-            <option value="brewpub">brewpub</option>
-            <option value="closed">closed</option>
+            ${typeOptions}
           </select>
           <button id="submitButton" type="submit">Submit</button>
           <div id="allBreweriesList"></div>
         </div>
         `
+
         document.querySelector('#app').insertAdjacentHTML('afterbegin', html);
         const breweriesList = document.getElementById('allBreweriesList');
         breweriesList.innerHTML = allBreweries(data);
@@ -61,19 +57,22 @@ function fetchData() {
 function allBreweries(data) {
   if (data.length === 0) {
     return `
-      <div>There is no brewery matching the selection</div>
+      <div id="emptySelect">There is no brewery matching the selection</div>
     `
   }
   return data
   .map(element => {
     let phone = element.phone;
+    if (element.phone !== null) {
+      phone = element.phone.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
+    }
     return `
-      <div class="container">
-        <div class="name">Name: ${element.name}</div>
-        <div class="type">Brewery Type: ${element.brewery_type}</div>
-        <div class="street">Street: ${element.street ? element.street : ''}</div>
-        <div class="phone">Phone: ${element.phone ? phone : ''}</div>
-        <div class="website">Website: ${element.website_url ? element.website_url : ''}</div>
+      <div class="card">
+        <div id="name">Name: ${element.name}</div>
+        <div id="type">Brewery Type: ${element.brewery_type}</div>
+        <div id="street">Street: ${element.street ? element.street : ''}</div>
+        <div id="phone">Phone: ${element.phone ? phone : ''}</div>
+        <div id="website">Website: ${element.website_url ? element.website_url : ''}</div>
       </div>
     `
   })
